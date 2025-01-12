81784217b9525aa4de71f550573df4bac49c0c8a
alexandradigiacomo <alexandra.digiacomo@stanford.edu>
2025-01-12 01:37:09 +0000

    new data loader, debugged

--- earth_obs_seg/dataset.py
+++ earth_obs_seg/dataset.py
@@ -1,94 +1,62 @@
-'''
-    PyTorch dataset class for COCO-CT-formatted datasets. Note that you could
-    use the official PyTorch MS-COCO wrappers:
-    https://pytorch.org/vision/master/generated/torchvision.datasets.CocoDetection.html
-
-    We just hack our way through the COCO JSON files here for demonstration
-    purposes.
-
-    See also the MS-COCO format on the official Web page:
-    https://cocodataset.org/#format-data
-
-    2022 Benjamin Kellenberger
-'''
-
+import yaml
 import os
 import json
+import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import Compose, Resize, ToTensor
 from PIL import Image
+import matplotlib.pyplot as plt
+from pycocotools import mask as coco_mask
+from pycocotools.coco import COCO
+import numpy as np
 
 class SharkBody(Dataset):
 
-    def __init__(self, cfg, split='train'):
-        '''
-            Constructor. Here, we collect and index the dataset inputs and
-            labels.
-        '''
-        self.data_root = cfg['data_root']
-        self.split = split
-        # Transforms. Here's where we could add data augmentation 
-        #  For now, we just resize the images to the same dimensions...and convert them to torch.Tensor.
-        #  For other transformations see Björn's lecture on August 11 or 
-        self.transform = Compose([              
-            Resize((cfg['image_size'])),        
-            ToTensor()                          
-        ])
-        
-        # index data into list
-        self.data = []
+    def __init__(self, cfg, split='train'): # collect and index dataset inputs/labels
+        self.data_root = cfg['data_root'] # root folder
+        self.split = split # determine split
 
-        # load annotation file
-        annoPath = os.path.join(
-            self.data_root,
-            'eccv_18_annotation_files',
-            'train_annotations.json' if self.split=='train' else 'cis_val_annotations.json'
-        )
-        meta = json.load(open(annoPath, 'r'))
-
-        # enable filename lookup. Creates image IDs and assigns each ID one filename. 
-        #  If your original images have multiple detections per image, this code assumes
-        #  that you've saved each detection as one image that is cropped to the size of the
-        #  detection, e.g., via megadetector.
-        images = dict([[i['id'], i['file_name']] for i in meta['images']])
-        # create custom indices for each category that start at zero. Note: if you have already
-        #  had indices for each category, they might not match the new indices.
-        labels = dict([[c['id'], idx] for idx, c in enumerate(meta['categories'])])
+        self.transform = Compose([ # transform: resize/convert to torch tensor and (opt.) augmentation      
+            Resize((cfg['image_size'], cfg['image_size'])), ToTensor()])
         
-        # since we're doing classification, we're just taking the first annotation per image and drop the rest
-        images_covered = set()      # all those images for which we have already assigned a label
-        for anno in meta['annotations']:
-            imgID = anno['image_id']
-            if imgID in images_covered:
-                continue
+        annotations_path = os.path.join( # path to annotations
+            self.data_root, 'annotations', 
+            'train.json' if self.split == 'train' else ('val.json' if self.split == 'val' else 'test.json')) # choose one of three splits
+
+        self.coco = COCO(annotations_path) 
+        self.data = [] # for storing masks
+        self.annotation_ids = self.coco.getAnnIds(catIds = [1])
+        self.annotations = self.coco.loadAnns(self.annotation_ids)
+        
+        for ann in self.annotations: # loop through annotations and store
+            image_id = ann['image_id']
+            file_name = self.coco.loadImgs(image_id)[0]['file_name']
+            binary_mask = self.coco.annToMask(ann)  
             
-            # append image-label tuple to data
-            imgFileName = images[imgID]
-            label = anno['category_id']
-            labelIndex = labels[label]
-            self.data.append([imgFileName, labelIndex])
-            images_covered.add(imgID)       # make sure image is only added once to dataset
-    
+            self.data.append({ # store image and mask data
+                'image_id': image_id,
+                'file_name': file_name,
+                'mask': Image.fromarray(binary_mask)
+            })
 
     def __len__(self):
-        '''
-            Returns the length of the dataset.
-        '''
         return len(self.data)
 
     
     def __getitem__(self, idx):
-        '''
-            Returns a single data point at given idx.
-            Here's where we actually load the image.
-        '''
-        image_name, label = self.data[idx]              # see line 57 above where we added these two items to the self.data list
 
-        # load image
-        image_path = os.path.join(self.data_root, 'eccv_18_all_images_sm', image_name)
-        img = Image.open(image_path).convert('RGB')     # the ".convert" makes sure we always get three bands in Red, Green, Blue order
+        image_name = self.data[idx]['file_name'] # pull file name
+        image_path = os.path.join(self.data_root, 'images', image_name) # pull image path
+        img = Image.open(image_path).convert('RGB')  # open image
+        mask = self.data[idx]['mask'] # pull mask
 
+        print(f"Original Image size: {img.size}")  # Debugging step: print original size
+
+        
         # transform: see lines 31ff above where we define our transformations
         img_tensor = self.transform(img)
+        mask_tensor = self.transform(mask)
 
-        return img_tensor, label
+        sample = dict(image=img_tensor, mask=mask_tensor)
+
+        return sample
